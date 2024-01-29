6d981788eaf463d5e5b8699f725ebe98bb08098b
Nyrvlivy <nyrvlivy@gmail.com>
2024-01-29 10:19:12 -0300

    chore: Initial project structure.

---
+++ src/main/java/br/com/financecontrolapi/FinanceControlApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.financecontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/financecontrolapi/FinanceControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

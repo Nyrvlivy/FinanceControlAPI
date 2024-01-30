0cbf54d0f54898119d6661413a8a9eb1decde49b
Nyrvlivy <nyrvlivy@gmail.com>
2024-01-30 14:06:44 -0300

    feat: Implement transaction update function and frontend integration.

--- src/main/java/br/com/financecontrolapi/api/v1/controller/TransactionController.java
+++ src/main/java/br/com/financecontrolapi/api/v1/controller/TransactionController.java
@@ -5,6 +5,9 @@ import br.com.financecontrolapi.api.v1.dto.TransactionListDTO;
 import br.com.financecontrolapi.api.v1.mapper.TransactionMapper;
 import br.com.financecontrolapi.business.services.TransactionService;
 import br.com.financecontrolapi.infrastructure.entities.TransactionEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +25,29 @@ public class TransactionController {
     private final TransactionService transactionService;
     private final TransactionMapper transactionMapper;
 
+    @Operation(summary = "Find all transactions", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search operation successful"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     @CrossOrigin
     @GetMapping
     public ResponseEntity<List<TransactionDTO>> findAllTransactions() {
         List<TransactionEntity> transactions = transactionService.findAllTransactions();
 
-        List<TransactionDTO> transactionDTOs = transactions.stream()
+        List<TransactionDTO> dtos = transactions.stream()
                 .map(transactionMapper::toDto)
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok(transactionDTOs);
+        return ResponseEntity.ok(dtos);
     }
 
+    @Operation(summary = "Save new transactions", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Save transactions operation successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
     @CrossOrigin
     @PostMapping
     public ResponseEntity<List<TransactionDTO>> createTransactions(@RequestBody TransactionListDTO transactionListDTO) {
@@ -43,10 +57,34 @@ public class TransactionController {
 
         List<TransactionEntity> createdTransactions = transactionService.createMultiTransactions(transactionEntities);
 
-        List<TransactionDTO> createdTransactionDTOs = createdTransactions.stream()
+        List<TransactionDTO> dtos = createdTransactions.stream()
                 .map(transactionMapper::toDto)
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok(createdTransactionDTOs);
+        return ResponseEntity.ok(dtos);
     }
+
+    @Operation(summary = "Update transactions", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update transaction operation successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Transaction not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid request data."),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity<TransactionDTO> updateTransaction(@PathVariable Integer id, @RequestBody TransactionDTO transactionDTO) {
+        TransactionEntity transaction = transactionService.findById(id);
+
+        transaction.setDate(transactionDTO.getDate());
+        transaction.setValue(transactionDTO.getValue());
+        transaction.setCategory(transactionDTO.getCategory());
+
+        TransactionEntity updatedTransaction = transactionService.save(transaction);
+        TransactionDTO dto = transactionMapper.toDto(updatedTransaction);
+
+        return ResponseEntity.ok(dto);
+    }
+
 }
--- src/main/java/br/com/financecontrolapi/business/services/TransactionService.java
+++ src/main/java/br/com/financecontrolapi/business/services/TransactionService.java
@@ -3,7 +3,9 @@ package br.com.financecontrolapi.business.services;
 import br.com.financecontrolapi.infrastructure.entities.TransactionEntity;
 import br.com.financecontrolapi.infrastructure.repositories.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -12,6 +14,11 @@ import java.util.List;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+
+    public TransactionEntity findById(Integer id) {
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Transaction not found"));
+    }
 
     public List<TransactionEntity> findAllTransactions() {
         return transactionRepository.findAll();
@@ -23,6 +30,10 @@ public class TransactionService {
 
     public List<TransactionEntity> createMultiTransactions(List<TransactionEntity> entities) {
         return transactionRepository.saveAll(entities);
+    }
+
+    public TransactionEntity save(TransactionEntity transaction) {
+        return transactionRepository.save(transaction);
     }
 
 

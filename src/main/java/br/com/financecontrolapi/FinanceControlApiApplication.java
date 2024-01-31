eda0915b2dad50a0b7ba2f053a71735c7e99996d
Nyrvlivy <nyrvlivy@gmail.com>
2024-01-30 23:02:55 -0300

    fix: Missing dotenv function param.

--- src/main/java/br/com/financecontrolapi/FinanceControlApiApplication.java
+++ src/main/java/br/com/financecontrolapi/FinanceControlApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinanceControlApiApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(FinanceControlApiApplication.class, args);
     }
